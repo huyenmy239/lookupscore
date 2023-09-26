1c6c04964df0d77dd2464a39034d2ef85f4b6d0d
huyenmy239 <92309591+huyenmy239@users.noreply.github.com>
2023-09-26 14:22:43 +0700

    Update files

--- lookupScore/settings.py
+++ lookupScore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7^yh$6p=u!0ro5p5v%ymuuvu4foy$^(^90^(x@x+*7fz1-004m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
